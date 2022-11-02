e7d6d72267ebb99aeec9675a9db19e372efe3319
matubara <matsubara.h@gmail.com>
2022-11-02 23:03:11 +0900

    機能修正
    直前にバックアップした最新データからリストアするように修正

--- LOLIPOP-SINCERETCHNOLOGY.BIZ_MAIN/sh/nc_restore.sh
+++ LOLIPOP-SINCERETCHNOLOGY.BIZ_MAIN/sh/nc_restore.sh
@@ -2,9 +2,9 @@
 
 SUBDOMAIN=storage2022
 
-BASEDIR=/home/users/2/main.jp-onlinestore/web/${SUBDOMAIN}.mementoful.com/
-#BACKUPDIR=/home/users/2/main.jp-onlinestore/transferred-backup/
-BACKUPDIR=/home/users/2/main.jp-onlinestore/web/${SUBDOMAIN}.mementoful.com/backup/
+BASEDIR=/home/users/2/main.jp-blogdeoshiete/web/${SUBDOMAIN}.sinceretechnology.net.au/
+#BACKUPDIR=/home/users/2/main.jp-blogdeoshiete/transferred-backup/
+BACKUPDIR=/home/users/2/main.jp-blogdeoshiete/web/${SUBDOMAIN}.sinceretechnology.net.au/backup/
 
 if [ -f ${BACKUPDIR}updatedflag.txt ]; then
 echo ok
@@ -16,7 +16,7 @@ else
 fi 
 
 #MAINTENANCE MODE OFF
-#/usr/local/php/7.4/bin/php  /home/users/2/main.jp-onlinestore/web/${SUBDOMAIN}.mementoful.com/nextcloud/occ maintenance:mode --on
+#/usr/local/php/7.4/bin/php  /home/users/2/main.jp-blogdeoshiete/web/${SUBDOMAIN}.sinceretechnology.net.au/nextcloud/occ maintenance:mode --on
 
 
 SYSDATE=`cat ${BACKUPDIR}latestfile.txt`_
@@ -25,10 +25,10 @@ echo "backup date=${SYSDATE}"
 #FILES
 tar -xf ${BACKUPDIR}${SYSDATE}nextcloud.tar
 tar -xf ${BACKUPDIR}${SYSDATE}data.tar
-cp -rp ./home/users/2/main.jp-onlinestore/web/storage2022.mementoful.com/nextcloud ${BASEDIR}
-cp -rp ./home/users/2/main.jp-onlinestore/web/storage2022.mementoful.com/data ${BASEDIR}
+cp -rp ./home/users/2/main.jp-blogdeoshiete/web/storage2022.sinceretechnology.net.au/nextcloud ${BASEDIR}
+cp -rp ./home/users/2/main.jp-blogdeoshiete/web/storage2022.sinceretechnology.net.au/data ${BASEDIR}
 cp -p ${BASEDIR}config.php ${BASEDIR}nextcloud/config 
 #DATABASE
-mysql -u LAA1228131 -pmelb1999 -h mysql205.phy.lolipop.lan LAA1228131-nextcloudmir < ${BACKUPDIR}${SYSDATE}nextcloud_db.sql
+mysql -u LAA1467345 -pmelb1999 -h mysql207.phy.lolipop.lan LAA1467345-storage2022 < ${BACKUPDIR}${SYSDATE}nextcloud_db.sql
 #MAINTENANCE MODE OFF
-#/usr/local/php/7.4/bin/php  /home/users/2/main.jp-onlinestore/web/docs.mementoful.com/nextcloud/occ maintenance:mode --off
+#/usr/local/php/7.4/bin/php  /home/users/2/main.jp-blogdeoshiete/web/docs.sinceretechnology.net.au/nextcloud/occ maintenance:mode --off
